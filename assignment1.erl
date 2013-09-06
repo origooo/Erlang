5ae2befe59a2460fe0bf6a7b06c141ace1b19a4f
Patrik Bäckström <mail@patrikbackstrom.com>
2013-09-06 15:36:02 +0200

    TEST: Updated assignment1.erl

--- assignment1.erl
+++ assignment1.erl
@@ -26,26 +26,33 @@ mul(0) ->
 mul(_N) -> ok.
 
 %% take_snd
-take_snd(_List) -> ok.
+take_snd([H | T]) when H /= [] ->
+    %[B || {_, B} <- List]; Assuming function head take_snd(List)
+    %[X | take_snd(T); Assuming function head take_snd([{_, X} | T])
+    [extract_element(H) | take_snd(T)];
+take_snd(_List) -> 
+    [].
+
+extract_element({_A, B}) ->
+    B.
 
 %% Fibonacci
 fib(Elements) ->
-    fib_helper(Elements, 0, 1, 1).
+    fib_helper(Elements, 0, 1).
 %fib(_N) -> ok.
 
-fib_helper(Elements, Counter, El1, El2) when Counter < Elements ->
-    %Num1 = El1,
-    %Num2 = El2,
-    El1 + El2 + fib_helper(Elements, Counter+1, El2, El1 + El2);
-fib_helper(_Elements, _Counter, _El1, _El2) ->
-    0.
-
+fib_helper(Elements, Sum, Next) when Elements > 0 ->
+    fib_helper(Elements - 1, Next, Sum + Next);
+fib_helper(0, Sum, _Next) ->
+    Sum.
 
 %% Efficient fib.
 %% eff_fib(_N) -> ok.
 
 %% Digitify a number
-digitize(_N) -> ok.
+digitize(N) when N >= 0 ->
+    [Digit || Digit <- N];
+digitize(_N) -> 0.
 
 %% is_happy
 is_happy(_N) -> ok.
